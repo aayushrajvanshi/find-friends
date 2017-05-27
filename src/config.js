4ea5c9af07d3b16162efdffd2a24bdf64bd6cd8c
Aayush Rajvanshi <aayushrajvanshii@gmail.com>
2017-05-27 12:33:43 +0530

    GCM Added

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     'secret': 'mysecret',
-    'database': 'mongodb://127.0.0.1:27017/api-db',
+    'database': 'mongodb://find-friends:f1ndFriend$@ds011785.mlab.com:11785/heroku_66ks7vlc',
     'server_key': 'AAAAa85WrJo:APA91bHYxaCJY3vLr9CN_r35z6H1pBOnEI18pwRrcwMVZVnvUX7KTqJwPDdmE1y8ko7ark3ihWakvgmpRr548lmH2I1AARx-ggBlsdAdxUakkcO1-fy_3X4qt3LMrh7LOUATMKqxz8fz'
 };
