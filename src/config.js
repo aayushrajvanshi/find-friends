60c07f3fe1df7da8357beda3691462266c859b19
Aayush Rajvanshi <aayushrajvanshii@gmail.com>
2017-05-29 21:20:08 +0530

    Server Key updated

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export default {
     'secret': 'mysecret',
     'database_dev': 'mongodb://127.0.0.1:27017/api-db',
     'database_prod': 'mongodb://find-friends:f1ndFriend$@ds011785.mlab.com:11785/heroku_66ks7vlc',
-    'server_key': 'AAAAa85WrJo:APA91bHYxaCJY3vLr9CN_r35z6H1pBOnEI18pwRrcwMVZVnvUX7KTqJwPDdmE1y8ko7ark3ihWakvgmpRr548lmH2I1AARx-ggBlsdAdxUakkcO1-fy_3X4qt3LMrh7LOUATMKqxz8fz'
+    'server_key': 'AAAAN0OmlUc:APA91bGc3jYA1Lyog8XGx7qSW1B_mIpHH3zci_7erYipTxzb_-6AcRW-oeLNeoSnrzKLVyDy9sRqPJzgrwPimIjkfJ03ApsBv3jVMWUzKaBfluVPLpryhOooMCc7AP3m5SePfRY46gbf'
 };
